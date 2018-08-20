0b3115d745ad64470924f846fe22172d98a559b5
Suzanne Su <suhysu@gmail.com>
2018-08-20 15:19:17 -0700

    phaser cdn link seems to work

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 'use strict';
+console.log ('javascript is linked');
 
-console.log('link');
+// create a new scene named "Game"
+let gameScene = new Phaser.Scene('Game');
+
+// our game's configuration
+let config = {
+  type: Phaser.AUTO, //Phaser will decide how to render our game (WebGL or Canvas)
+  width: 800, // game width
+  height: 600, // game height
+  scene: gameScene // our newly created scene
+};
+
+// create the game, and pass it the configuration
+let game = new Phaser.Game(config);
+
