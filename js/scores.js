a78883e99ddaed5e21014b95efcf70bac441802e
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-08-21 16:27:43 -0700

    Add starter code to scores.js

--- js/scores.js
+++ js/scores.js
@@ -1,3 +1,22 @@
 'use-strict';
 
 console.log('the js is linked');
+
+// trigger when player finishes game
+// calculate high score by a multiplier
+function calculateScore(timeInSec) {
+  return timeInSec * 10;
+}
+
+function saveScore() {
+  // add score to local storage scores
+}
+
+function sortScores() {
+
+}
+
+function displayScores() {
+  // sort and display all scores in local storage
+  // can change to Top X later
+}
