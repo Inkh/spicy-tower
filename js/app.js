a8612d774ab54e5aad924bdf543debb632356fee
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-08-21 17:10:35 -0700

    Update score save

--- js/app.js
+++ js/app.js
@@ -122,7 +122,7 @@ gameScene.replay = function(){
 gameScene.preload = function() {
   this.load.image('sprite', 'assets/dead.png', { frameWidth: 32, frameHeight: 48 });
   this.load.image('tile', 'assets/15-01.png');
-}
+};
 
 //Global variable for key input
 var cursors;
@@ -202,7 +202,7 @@ function ender(){
   sprite.setVelocityY(-500);
   sprite.body.gravity.y = 0;
   sprite.setCollideWorldBounds(false);
-
+  clearTimeout(t);
 }
 
 // function restart(){
@@ -227,12 +227,25 @@ function timer(){
   t = setTimeout(addTime, 1000);
 }
 
-document.addEventListener("keydown", startGame);
+document.addEventListener('keydown', startGame);
 
 function startGame(){
   if(event.which){
     console.log(event.which);
     timer();
-    document.removeEventListener("keydown", startGame);
+    document.removeEventListener('keydown', startGame);
   }
 }
+
+// // trigger when player finishes game
+// // calculate high score by a multiplier
+// function calculateScore(timeInSec) {
+//   return timeInSec * 10;
+// }
+
+// function saveScore(t) {
+//   // add score to local storage scores
+//   const userScores = [];
+//   userScores.push({ user: user, score: calculateScore(timeInSec) });
+//   localStorage.setItem('userScores', userScores);
+// }
--- js/scores.js
+++ js/scores.js
@@ -4,22 +4,40 @@ console.log('the js is linked');
 
 // trigger when player finishes game
 // calculate high score by a multiplier
-function calculateScore(timeInSec) {
-  return timeInSec * 10;
+function calculateScore(t) {
+  return t * 10;
 }
 
-function saveScore(user, timeInSec) {
+function saveScore(user, t) {
   // add score to local storage scores
   const userScores = [];
-  userScores.push({ user: user, score: calculateScore(timeInSec) });
+  userScores.push({ user: user, score: calculateScore(t) });
   localStorage.setItem('userScores', userScores);
 }
 
+// accepts JSON parsed user scores as a paramater
 function sortScores(userScores) {
-  userScores.sort((a, b) => b.score > a.score);
+  return userScores.sort((a, b) => b.score - a.score);
 }
 
+// can change to Top X later
 function displayScores() {
+  // grab and parse JSON user scores string back to JS
+  let parsed = JSON.parse(localStorage.getItem('userScores'));
   // sort and display all scores in local storage
-  // can change to Top X later
+  const sorted = sortScores(parsed);
+
+  // create HTML element
+  const newEl = document.createElement('div');
 }
+
+function createEl(type, content = null, klass = null) {
+  let el = document.createElement(type);
+  if (content) { el.textContent = content; }
+  if (klass) { el.class = klass; }
+
+  return el;
+}
+
+// runner code
+saveScore(user, timeInSec);
