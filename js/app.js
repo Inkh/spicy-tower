7b2c04f9a8523e727c35925ba3ce0633d8743d17
brinel321 <brian.c.nelson1@outlook.com>
2018-08-22 09:48:10 -0700

    jumper now falls from the top left corner of the game board'

--- js/app.js
+++ js/app.js
@@ -150,7 +150,7 @@ gameScene.create = function() {
         tile.displayWidth = 120;
         tile.displayHeight = 20;
       } else if (gameMap[i][j] === 2) {
-        sprite = this.physics.add.sprite(200, 100, 'sprite');
+        sprite = this.physics.add.sprite(795, 0, 'sprite');
         //Set gravity to player sprite only
         sprite.body.gravity.y = 500;
         sprite.body.setSize(0, 500);
@@ -202,7 +202,7 @@ function ender(){
   sprite.setVelocityY(-500);
   sprite.body.gravity.y = 0;
   sprite.setCollideWorldBounds(false);
-
+  clearTimeout(t);
 }
 
 // function restart(){
@@ -212,8 +212,6 @@ function ender(){
 var re = document.getElementById('restart');
 re.addEventListener('click', gameScene.replay);
 var h1 = document.getElementsByTagName('h1')[0];
-var start = document.getElementById('play');
-var tryagain = document.getElementById('tryagain');
 var seconds = 0;
 var t;
 
