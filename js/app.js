44191cbd8c2e8ff0881d78135ef99febe0573ce1
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-08-23 16:04:16 -0700

    Add landing page modal and game instructions

--- js/app.js
+++ js/app.js
@@ -186,8 +186,6 @@ gameScene.create = function() {
   player.body.checkCollision.left = false;
   player.body.checkCollision.right = false;
 
-  var lookingRight = true;
-
   // Idle animation creation
   this.anims.create({
     key: 'idle',
@@ -286,7 +284,7 @@ function ender(){
 // var re = document.getElementById('restart');
 // re.addEventListener('click', gameScene.replay);
 
-var h1 = document.getElementsByTagName('h1')[0];
+var h1 = document.querySelector('#time');
 var seconds = 0;
 var t;
 
@@ -303,6 +301,11 @@ function timer(){
 document.addEventListener('keydown', startGame);
 
 function startGame(){
+  // change canvas to visible & .game-instructions div to hidden
+  document.getElementsByTagName('canvas')[0].style.display = 'block';
+  document.querySelector('.game-instructions').style.display = 'none';
+  document.querySelector('#time').style.display = 'block';
+
   if(event.which){
     timer();
     musicPlayer();
