56ab1f14718351b1b3e489997e71526e5077bc32
Jeff <jhwe92@gmail.com>
2018-08-23 16:27:51 -0700

    Added custom sprite for platform

--- js/app.js
+++ js/app.js
@@ -136,7 +136,7 @@ gameScene.replay = function(){
 };
 
 gameScene.preload = function() {
-  this.load.image('tile', 'assets/15-01.png');
+  this.load.image('tile', 'assets/platform.png');
   this.load.spritesheet('red', 'assets/red-sprites.png', { frameWidth: 50, frameHeight: 50 });
   this.load.image('coin', 'assets/coin2.png'); //{ frameWidth: 50, frameHeight: 50 });
   this.load.spritesheet('lava', 'assets/lava-fall.png', { frameWidth: 50, frameHeight: 50 });
